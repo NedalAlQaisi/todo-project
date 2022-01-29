18eb5b97392be1de243318cbbb548e2d3a983e36
NedalAlQaisi <n.alqaisi221999@gmail.com>
2022-01-29 16:08:19 +0200

    Fix code yes/no questions

--- js/app.js
+++ js/app.js
@@ -1,23 +1,33 @@
+/*
 //The asking user to enter his/her name as input.
 
-var YourName = 
-prompt("What is your name?");
+var YourName =
+    prompt("What is your name?");
 
 //The asking user to enter his/her gender as input.
 
-var YourGender = 
-prompt("Your gender [Male] OR [Female] ?");
+var YourGender =
+    prompt("Your gender [Male] OR [Female] ?");
+
+if (YourGender.toUpperCase() == "MALE" && YourGender.toUpperCase() == "FEMALE") {
+    alert("Hallo Mr." + YourName);
+} else if (YourGender.toUpperCase() == "FEMALE") {
+    alert("Hallo Ms." + YourName);
+
+} else {
+    alert("Hallo" + YourName);
+}
 
 //The asking user to enter his/her age as input.
 
-var YourAge = 
-prompt("Enter your age please.\nYou must be over 18 years old.");
+var YourAge =
+    prompt("Enter your age please.\nYou must be over 18 years old.");
 
 // Exception if an age error occurs.
 
-if(YourAge <= 0) {
+if (YourAge <= 0) {
     alert("Error: your age is equal 0 OR less");
-} else{
+} else {
     alert("Next");
 
 }
@@ -25,52 +35,52 @@ if(YourAge <= 0) {
 //The asking user to confirm if he wants to skip the welcoming message.
 
 var Skip =
-confirm("Do you want to skip?");
+    confirm("Do you want to skip?");
 
 
 //welcoming message with the informetion if NOT skip
 
-if (Skip == false){
+if (Skip == false) {
 
-    if (YourGender.toUpperCase() == "MALE" ){
-        alert("Hallo Mr." + YourName );
-    } else if (YourGender.toUpperCase() == "FEMALE"){
-        alert("Hallo Ms." + YourName );
+    if (YourGender.toUpperCase() == "MALE") {
+        alert("Hallo Mr." + YourName);
+    } else if (YourGender.toUpperCase() == "FEMALE") {
+        alert("Hallo Ms." + YourName);
 
     } else {
-        alert("Hallo" + YourName );
+        alert("Hallo" + YourName);
     }
 
 }
 
 
-
+*/
 
 //add new three (Yes/No) questions.
 
-function askQuestion(question){
-    let answer = prompt (question);
-     console.log(answer);
-     if(answer == "yes" || answer == "no"){
+function askQuestion(question) {
+    let answer = prompt(question);
+    console.log(answer);
+    if (answer.toUpperCase() == "YES" || answer.toUpperCase() == "NO") {
 
-         return answer;
+        return (answer);
 
-       } else if(answer == ""){
+    } else {
 
-          return "Invalid"
-       }
-}   
- let theQ_1 = askQuestion("Do you like programming?");
- let theQ_2 = askQuestion("Do you like to drink coffee during long work?");
- let theQ_3 = askQuestion("Can you handle working under pressure?");
-                 
- let answersArr = [theQ_1 , theQ_2 , theQ_3];
+        return ("Invalid");
+    }
+}
+let theQ_1 = askQuestion("Do you like programming?");
+let theQ_2 = askQuestion("Do you like to drink coffee during long work?");
+let theQ_3 = askQuestion("Can you handle working under pressure?");
 
- console.log(answersArr);
+let answersArr = [theQ_1, theQ_2, theQ_3];
+
+console.log(answersArr);
 
 
- let traverse = (arr) =>{
-    for (let i = 0 ; i < arr.length ; i++){
+let traverse = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
         console.log(arr[i]);
     }
 }
